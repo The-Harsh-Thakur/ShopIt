97e1508f9a3a3d5743ce719622d658ff3f62af6b
Harsh Thakur <harshthakur12092002@gmail.com>
2025-10-09 12:05:33 +0530

    initial project setup

---
+++ src/test/java/com/harsh/ecom/EcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
