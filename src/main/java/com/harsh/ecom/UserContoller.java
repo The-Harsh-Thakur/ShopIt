1bf47005ac43e4af0bd92ac3e43de4f18e5ea237
The-Harsh-Thakur <harshthakur12092002@gmail.com>
2026-01-10 18:14:14 +0530

    initial user setup complete

--- src/main/java/com/harsh/ecom/UserContoller.java
+++ src/main/java/com/harsh/ecom/UserContoller.java
@@ -1,6 +1,7 @@
 package com.harsh.ecom;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,18 +14,30 @@ public class UserContoller {
     private final UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/api/users/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userService.fetchUser(id);
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.fetchUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/api/users/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User updatedUser)
+    {
+        boolean updated = userService.updateUser(id, updatedUser);
+
+        if(updated)
+            return ResponseEntity.ok("User updated sucessfully");
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping("/api/users")
-    public String createUser(@RequestBody User user) {
+    public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.addUser(user);
-        return "User Added Successfully";
+        return ResponseEntity.ok("User Added Successfully");
     }
 }
