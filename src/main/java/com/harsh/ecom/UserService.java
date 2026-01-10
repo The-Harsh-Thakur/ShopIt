c86e5e3481fb818e41157efca9017296ab260202
The-Harsh-Thakur <harshthakur12092002@gmail.com>
2026-01-10 21:43:07 +0530

    added spring-jpa with h2

--- src/main/java/com/harsh/ecom/UserService.java
+++ src/main/java/com/harsh/ecom/UserService.java
@@ -1,5 +1,6 @@
 package com.harsh.ecom;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -8,35 +9,30 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private List<User> userList = new ArrayList<>();
-
-    private Long id = 1L;
+    private final UserRepository userRepository;
+    //private List<User> userList = new ArrayList<>();
 
     public List<User> getAllUsers() {
-        return userList;
+        return userRepository.findAll();
     }
 
-    public List<User> addUser(@RequestBody User user) {
-        user.setId(id++);
-        userList.add(user);
-        return userList;
+    public void addUser(@RequestBody User user) {
+        userRepository.save(user);
     }
 
     public Optional<User> fetchUser(Long id) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst();
+        return userRepository.findById(id);
     }
 
     public boolean updateUser(Long id, User updatedUser)
     {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
+        return userRepository.findById(id)
                 .map(existingUser -> {
                     existingUser.setFirstName(updatedUser.getFirstName());
                     existingUser.setLastName(updatedUser.getLastName());
+                    userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
     }
