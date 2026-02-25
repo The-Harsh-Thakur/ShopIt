96efc63babbd8b5aff74228ad5ea40a4e71343cb
The-Harsh-Thakur <harshthakur12092002@gmail.com>
2026-02-25 14:59:08 +0530

    converted order into a microservice

---
+++ order/src/main/java/com/ecom/order/dto/OrderItemDTO.java
@@ -0,0 +1,19 @@
+package com.ecom.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class OrderItemDTO {
+    private Long id;
+    private String productId;
+    private Integer quantity;
+    private BigDecimal price;
+    private BigDecimal subTotal;
+
+    public OrderItemDTO(Long id, Long productId, Integer quantity, BigDecimal price, BigDecimal multiply) {
+    }
+}
