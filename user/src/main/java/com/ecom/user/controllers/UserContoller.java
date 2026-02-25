2a2adf780ae84cba124f8c8db89471f02fc35edc
The-Harsh-Thakur <harshthakur12092002@gmail.com>
2026-02-25 13:33:38 +0530

    made user as a microservice

---
+++ user/src/main/java/com/ecom/user/controllers/UserContoller.java
@@ -0,0 +1,45 @@
+package com.ecom.user.controllers;
+
+import com.ecom.user.dto.UserRequest;
+import com.ecom.user.dto.UserResponse;
+import com.ecom.user.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserContoller {
+
+    private final UserService userService;
+
+    @GetMapping("/api/users")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
+        return userService.fetchUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/api/users/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody UserRequest updatedUserRequest)
+    {
+        boolean updated = userService.updateUser(id, updatedUserRequest);
+
+        if(updated)
+            return ResponseEntity.ok("User updated sucessfully");
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/users")
+    public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest) {
+        userService.addUser(userRequest);
+        return ResponseEntity.ok("User Added Successfully");
+    }
+}
---
+++ user/src/main/java/com/ecom/user/services/UserService.java
@@ -0,0 +1,87 @@
+package com.ecom.user.services;
+
+
+import com.ecom.user.dto.AddressDTO;
+import com.ecom.user.dto.UserRequest;
+import com.ecom.user.dto.UserResponse;
+import com.ecom.user.models.Address;
+import com.ecom.user.models.User;
+import com.ecom.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    //private List<User> userList = new ArrayList<>();
+
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(this::maptoUserResponse)
+                .collect(Collectors.toList());
+    }
+
+    public void addUser(UserRequest userRequest) {
+        User user = new User();
+        updateUserFromRequest(user, userRequest);
+        userRepository.save(user);
+    }
+
+    public Optional<UserResponse> fetchUser(Long id) {
+        return userRepository.findById(id)
+                .map(this::maptoUserResponse);
+    }
+
+    public boolean updateUser(Long id, UserRequest updatedUserRequest)
+    {
+        return userRepository.findById(id)
+                .map(existingUser -> {
+                    updateUserFromRequest(existingUser, updatedUserRequest);
+                    userRepository.save(existingUser);
+                    return true;
+                }).orElse(false);
+    }
+
+    private void updateUserFromRequest(User user, UserRequest userRequest) {
+        user.setFirstName(userRequest.getFirstName());
+        user.setLastName(userRequest.getLastName());
+        user.setEmail(userRequest.getEmail());
+        user.setPhone(userRequest.getPhone());
+
+        if(userRequest.getAddress() != null) {
+            Address address = new Address();
+            address.setCity(userRequest.getAddress().getCity());
+            address.setCountry(userRequest.getAddress().getCountry());
+            address.setStreet(userRequest.getAddress().getStreet());
+            address.setZipcode(userRequest.getAddress().getZipcode());
+            address.setState(userRequest.getAddress().getState());
+        }
+    }
+
+    private UserResponse maptoUserResponse(User user) {
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(String.valueOf(user.getId()));
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setPhone(user.getPhone());
+        user.setRole(user.getRole());
+
+        if (user.getAddress() != null) {
+            AddressDTO addressDTO = new AddressDTO();
+            addressDTO.setStreet(user.getAddress().getStreet());
+            addressDTO.setCity(user.getAddress().getCity());
+            addressDTO.setState(user.getAddress().getState());
+            addressDTO.setCountry(user.getAddress().getCountry());
+            addressDTO.setZipcode(user.getAddress().getZipcode());
+            userResponse.setAddress(addressDTO);
+        }
+
+        return userResponse;
+    }
+}
